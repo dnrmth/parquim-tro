60f91ac777554499bbdf4129706ff4c429126273
Ana Genova <anagenova9@outlook.com>
2024-12-14 10:28:01 -0300

    Removing Inspector and Creating Vehicle

---
+++ inspector/src/main/java/com/parquimetro/inspector/controller/ParkingController.java
@@ -0,0 +1,27 @@
+package com.parquimetro.inspector.controller;
+
+import com.parquimetro.inspector.controller.dto.VehicleDto;
+import com.parquimetro.inspector.service.ParkingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/parking")
+public class ParkingController {
+
+    private final ParkingService parkingService;
+
+    @Autowired
+    public ParkingController(ParkingService parkingService) {
+        this.parkingService = parkingService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Long> saveVehicleParking(@RequestBody VehicleDto vehicleDto) {
+        return ResponseEntity.ok(parkingService.saveVehicleParking(vehicleDto));
+    }
+}
--- inspector/src/main/java/com/parquimetro/inspector/service/ParkingService.java
+++ inspector/src/main/java/com/parquimetro/inspector/service/ParkingService.java
@@ -1,7 +1,38 @@
 package com.parquimetro.inspector.service;
 
+import ch.qos.logback.core.util.StringUtil;
+import com.parquimetro.inspector.controller.dto.VehicleDto;
+import com.parquimetro.inspector.data.ParkingEntity;
+import com.parquimetro.inspector.exception.ParkingException;
+import com.parquimetro.inspector.repository.ParkingRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class ParkingService {
+
+    private final ParkingRepository parkingRepository;
+
+    @Autowired
+    public ParkingService(ParkingRepository parkingRepository) {
+        this.parkingRepository = parkingRepository;
+    }
+
+    @Transactional
+    public Long saveVehicleParking(VehicleDto vehicleDto) {
+        if (StringUtil.isNullOrEmpty(vehicleDto.plate())) {
+            throw new ParkingException("Placa inválida");
+        }
+
+        if (vehicleDto.initialDateTime().isAfter(vehicleDto.finalDateTime())) {
+            throw new ParkingException("Data inicial inválida");
+        }
+
+        ParkingEntity parkingEntity = new ParkingEntity(vehicleDto);
+        return parkingRepository.save(parkingEntity).getId();
+    }
 }
