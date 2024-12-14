fccdc7eb16b43375f8fd06850821b056e76a9d0e
Ana Genova <anagenova9@outlook.com>
2024-12-14 11:57:27 -0300

    Swagger annotations

--- inspector/src/main/java/com/parquimetro/inspector/controller/ParkingController.java
+++ inspector/src/main/java/com/parquimetro/inspector/controller/ParkingController.java
@@ -2,6 +2,9 @@ package com.parquimetro.inspector.controller;
 
 import com.parquimetro.inspector.controller.dto.VehicleDto;
 import com.parquimetro.inspector.service.ParkingService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,7 +23,16 @@ public class ParkingController {
         this.parkingService = parkingService;
     }
 
-
+    @Operation(
+            summary = "Salvar estacionamento",
+            description = """
+                    A chamada deste endpoint é feita diretamente pelo serviço de veículos após o pagamento da
+                    taxa do estacionamento rotativo
+                    """)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Anotações do estacionamento salva com sucesso"),
+            @ApiResponse(responseCode = "500", description = "Erro nas informações inseridas")
+    })
     @PostMapping
     public ResponseEntity<Long> saveVehicleParking(@RequestBody VehicleDto vehicleDto) {
         return ResponseEntity.ok(parkingService.saveVehicleParking(vehicleDto));
